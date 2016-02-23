d27b4661db6fd3df90f05b872a0ae9da37eb3483
DarkholmeTenk <shaney.booth@gmail.com>
2016-02-23 16:06:36 +0000

    Added keystone receptacle CC peripheral

--- src/main/java/am2/interop/CCInterop.java
+++ src/main/java/am2/interop/CCInterop.java
@@ -7,7 +7,9 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import am2.blocks.tileentities.TileEntityKeystoneRecepticle;
 import am2.blocks.tileentities.TileEntityLectern;
+import am2.items.ItemsCommonProxy;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Optional;
 import dan200.computercraft.api.ComputerCraftAPI;
@@ -41,7 +43,110 @@ public class CCInterop implements IPeripheralProvider
 		TileEntity te = world.getTileEntity(x, y, z);
 		if(te instanceof TileEntityLectern)
 			return new LecternPeripheral((TileEntityLectern)te);
+		if(te instanceof TileEntityKeystoneRecepticle)
+			return new GatewayPeripheral((TileEntityKeystoneRecepticle) te);
 		return null;
+	}
+
+	@Optional.Interface(iface="dan200.computercraft.api.peripheral.IPeripheral",modid="ComputerCraft")
+	private class GatewayPeripheral implements IPeripheral
+	{
+		private final TileEntityKeystoneRecepticle recepticle;
+
+		private GatewayPeripheral(TileEntityKeystoneRecepticle rec)
+		{
+			recepticle = rec;
+		}
+
+		@Override
+		public String getType()
+		{
+			return "am2.gateway";
+		}
+
+		private final String[] methods = new String[]{"isOpenable","isOpen","open","close"};
+		@Override
+		public String[] getMethodNames()
+		{
+			return methods;
+		}
+
+		private Object[] isOpenable()
+		{
+			return new Object[]{recepticle.canActivate()};
+		}
+
+		private Object[] isOpen()
+		{
+			return new Object[]{recepticle.isActive()};
+		}
+
+		private Object[] open(Object[] args)
+		{
+			if(args.length != 3) return new Object[]{"Expected 3 numeric args"};
+			long k = 0;
+			int i = 0;
+			for(Object o : args)
+			{
+				if((o != null) && !(o instanceof Double)) return new Object[]{"All arguments must be nil or a number >=0, <=16"};
+				if(o == null) continue;
+				int a = (int)Math.floor((Double)o);
+				if((a < 0) || (a >= 16)) return new Object[]{"Expected numeric args >=0, <=16"};
+				long ki = ItemsCommonProxy.rune.getKeyIndex(a);
+				ki = ki << ((i++) << 4);
+				k |= ki;
+			}
+			if(recepticle.isActive()) return new Object[]{"Already open"};
+			if(!recepticle.canActivate()) return new Object[]{"Invalid structure"};
+			recepticle.setActive(k);
+			return new Object[]{true};
+		}
+
+		private Object[] close()
+		{
+			if(recepticle.isActive())
+			{
+				recepticle.deactivate();
+				return new Object[]{true};
+			}
+			else
+				return new Object[]{false};
+		}
+
+		@Override
+		public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws LuaException, InterruptedException
+		{
+			switch(method)
+			{
+				case 0: return isOpenable();
+				case 1: return isOpen();
+				case 2: return open(arguments);
+				case 3: return close();
+				default: return null;
+			}
+		}
+
+		@Override
+		public void attach(IComputerAccess computer)
+		{
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void detach(IComputerAccess computer)
+		{
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public boolean equals(IPeripheral other)
+		{
+			if(other instanceof GatewayPeripheral)
+				return ((GatewayPeripheral)other).recepticle.equals(recepticle);
+			return false;
+		}
 	}
 
 	@Optional.Interface(iface="dan200.computercraft.api.peripheral.IPeripheral",modid="ComputerCraft")
